a77c173186787c9b7484ffc864177f845e61bdbf
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-26 11:09:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-// complete the given function
 
-function palindrome(str){
-
+function palindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  let cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+  // Compare characters from both ends of the string
+  let start = 0;
+  let end = cleanStr.length - 1;
+  
+  while (start < end) {
+    if (cleanStr[start] !== cleanStr[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  
+  return true;
 }
-module.exports = palindrome
+
+module.exports = palindrome;
+
